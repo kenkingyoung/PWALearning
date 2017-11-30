e0231517225b910374cbe8f6e071896b81fdb8dc
杨根青 <kenkingyoung@users.noreply.github.com>
2017-11-30 17:52:49 +0800

    update

--- sw2.js
+++ sw2.js
@@ -1,5 +1,5 @@
 self.addEventListener('fetch', function(event) {
-    if (/\.jpg$/.test(event.request.url)) {
+    if (/\.other.html$/.test(event.request.url)) {
         event.respondWith(
             new Response('<p>This is a response that comes from your service worker!</p>', { headers: { 'Content-Type': 'text/html' } })
         );
