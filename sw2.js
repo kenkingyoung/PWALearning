368337e0a87b72d7e08fd0c6b127e5ab891dca9d
杨根青 <kenkingyoung@users.noreply.github.com>
2017-11-30 17:44:33 +0800

    update

--- sw2.js
+++ sw2.js
@@ -1,5 +1,4 @@
 self.addEventListener('fetch', function(event) {
-    alert(event.request.url);
     if (/\.jpg$/.test(event.request.url)) {
         event.respondWith(fetch('images/unicorn.jpg'));
     }
