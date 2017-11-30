76405c951661802ddd5e9c08e76ec6621d19d356
杨根青 <kenkingyoung@users.noreply.github.com>
2017-11-30 17:48:05 +0800

    update

--- sw2.js
+++ sw2.js
@@ -1,5 +1,7 @@
 self.addEventListener('fetch', function(event) {
     if (/\.jpg$/.test(event.request.url)) {
-        event.respondWith(fetch('images/unicorn.jpg'));
+        event.respondWith(
+            new Response('<p>This is a response that comes from your service worker!</p>', { headers: { 'Content-Type': 'text/html' } })
+        );
     }
 });
