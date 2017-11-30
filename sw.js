cb3e9d57596f0b7a64f4d14c8687da1951d84c70
杨根青 <kenkingyoung@users.noreply.github.com>
2017-11-30 14:30:21 +0800

    change

--- sw.js
+++ sw.js
@@ -1,8 +1,30 @@
-// 为 fetch 事件添加事件监听器
-self.addEventListener('fetch', function(event) {
-    // 检查传入的 HTTP 请求是否是 JEPG 类型的图片
-    if (/\.jpg$/.test(event.request.url)) {
-        // 尝试获取 unicorn.jpg 用它替换图片来相应请求
-        event.responseWith(fetch('/images/unicorn.jpg'));
-    }
+// 缓存的名称
+// 我们将进入 Service Worker 的安装事件
+// 使用我们指定的缓存名称来打开缓存
+// 把 JavaScript 和 图片文件添加到缓存中
+var cacheName = 'helloWorld';
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => cache.addAll([
+            'js/scripts.js',
+            'images/hello.jpeg'
+        ]))
+    );
+});
+
+// 添加 fetch 事件的事件监听器
+// 检查传入的请求 URL 是否匹配当前缓存中存在的任何内容
+// 如果有 response 并且它不是 undefined 或 null 的话就将它返回
+// 否则只是如往常一样继续，通过网络获取预期的资源
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
 });
